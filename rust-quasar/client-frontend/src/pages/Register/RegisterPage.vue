c55083e15453a2ef861430a029e440d8ad0f72f9
vladiere <ruthakaza@gmail.com>
2024-08-27 10:21:20 +0800

    initial commit

---
+++ rust-quasar/client-frontend/src/pages/Register/RegisterPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    this is the register page
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  // name: 'PageName'
+});
+</script>
